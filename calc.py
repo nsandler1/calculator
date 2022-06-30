6b59622aec443c62c11ab31a12c8e157e5f9dc1d
NoahSandler <Noahsandler@gmail.com>
2022-06-30 18:44:46 -0400

    complete first two rounds of parsing

--- calc.py
+++ calc.py
@@ -1,12 +1,15 @@
+from copy import deepcopy
 import tkinter
 from tkinter import *
 from tkinter import ttk
 
 from enum import Enum, auto
+from functools import partial
 
-DIM_FRAME = "450x450"
+GUI_DIM = "450x450"
 BUTTON_WIDTH = 2
 BUTTON_FONT_SIZE = 32
+DISP_WIDTH = 10 # in character widths
 
 class Calculator:
     class Operation(Enum):
@@ -14,10 +17,9 @@ class Calculator:
         ADD = "+"
         SUBTRACT = "-"
         DIVIDE = "/"
-        MULTIPLY = "X"
+        MULTIPLY = "x"
         DECIMAL = "."
         EQUALS = "="
-        CLEAR = "C"
 
         def __str__(self):
             return self.value
@@ -27,114 +29,59 @@ class Calculator:
             return cls.NUMBER
 
     def __init__(self):
-        self.queue = []
-        self.label_text = None
+        self.memory = []
+        self.label = None
+        self.label_text = ""
         self.gui = self.init_gui()
         self.gui.mainloop()
 
     def init_gui(self):
         gui = Tk()
-        gui.geometry(DIM_FRAME)
+        gui.geometry(GUI_DIM)
         display = ttk.Frame(gui, padding=10)
-        btns = ttk.Frame(gui, padding=10)
+        keypad = ttk.Frame(gui, padding=10)
         display.grid()
-        btns.grid()
+        keypad.grid()
 
-        self.label_text = StringVar(display, "test")
         ttk.Style().configure('TButton', font=('Helvetica', BUTTON_FONT_SIZE))
         ttk.Style().configure('TLabel', font=('Helvetica', BUTTON_FONT_SIZE))
-        ttk.Label(display, relief="solid", textvariable=self.label_text).grid(column=0, row=0, columnspan=8, sticky=tkinter.W+tkinter.E)
+        label = ttk.Label(display, relief="solid", width=DISP_WIDTH, text="")
+        label.grid(column=0, row=0)
 
         kwargs = {
             "width": BUTTON_WIDTH
         }
 
-        def update_disp(val):
-            operation = self.Operation(val)
-            if operation is self.Operation.CLEAR:
-                self.label_text = StringVar(display, "aa")
-            else:
-                self.label_text = StringVar(display, str(val))
+        ttk.Button(keypad, text="C", command=self.clear, **kwargs).grid(column=0, row=0)
+        ttk.Button(keypad, text="(", command=partial(self.update_disp, "("), **kwargs).grid(column=1, row=0)
+        ttk.Button(keypad, text=")", command=partial(self.update_disp, ")"), **kwargs).grid(column=2, row=0)
+        ttk.Button(keypad, text="/", command=partial(self.update_disp, "/"), **kwargs).grid(column=3, row=0)
 
-        def clear():
-            update_disp("C")
+        ttk.Button(keypad, text="7", command=partial(self.update_disp, "7"), **kwargs).grid(column=0, row=1)
+        ttk.Button(keypad, text="8", command=partial(self.update_disp, "8"), **kwargs).grid(column=1, row=1)
+        ttk.Button(keypad, text="9", command=partial(self.update_disp, "9"), **kwargs).grid(column=2, row=1)
+        ttk.Button(keypad, text="X", command=partial(self.update_disp, "x"), **kwargs).grid(column=3, row=1)
 
-        def divide():
-            update_disp("/")
+        ttk.Button(keypad, text="4", command=partial(self.update_disp, "4"), **kwargs).grid(column=0, row=2)
+        ttk.Button(keypad, text="5", command=partial(self.update_disp, "5"), **kwargs).grid(column=1, row=2)
+        ttk.Button(keypad, text="6", command=partial(self.update_disp, "6"), **kwargs).grid(column=2, row=2)
+        ttk.Button(keypad, text="-", command=partial(self.update_disp, "-"), **kwargs).grid(column=3, row=2)
 
-        def mult():
-            update_disp("X")
+        ttk.Button(keypad, text="1", command=partial(self.update_disp, "1"), **kwargs).grid(column=0, row=3)
+        ttk.Button(keypad, text="2", command=partial(self.update_disp, "2"), **kwargs).grid(column=1, row=3)
+        ttk.Button(keypad, text="3", command=partial(self.update_disp, "3"), **kwargs).grid(column=2, row=3)
+        ttk.Button(keypad, text="+", command=partial(self.update_disp, "+"), **kwargs).grid(column=3, row=3)
 
-        def sub():
-            update_disp("-")
-
-        def add():
-            update_disp("+")
-
-        def decimal():
-            update_disp(".")
-
-        def one():
-            update_disp("1")
-
-        def two():
-            update_disp("2")
-
-        def three():
-            update_disp("3")
-
-        def four():
-            update_disp("4")
-
-        def five():
-            update_disp("5")
-
-        def six():
-            update_disp("6")
-
-        def seven():
-            update_disp("7")
-
-        def eight():
-            update_disp("8")
-
-        def nine():
-            update_disp("9")
-
-        def zero():
-            update_disp("0")
-
-
-        ttk.Button(btns, text="C", command=clear, **kwargs).grid(column=0, row=0)
-        ttk.Button(btns, text="(", command=gui.destroy, **kwargs).grid(column=1, row=0)
-        ttk.Button(btns, text=")", command=gui.destroy, **kwargs).grid(column=2, row=0)
-        ttk.Button(btns, text="/", command=divide, **kwargs).grid(column=3, row=0)
-
-        ttk.Button(btns, text="7", command=seven, **kwargs).grid(column=0, row=1)
-        ttk.Button(btns, text="8", command=eight, **kwargs).grid(column=1, row=1)
-        ttk.Button(btns, text="9", command=nine, **kwargs).grid(column=2, row=1)
-        ttk.Button(btns, text="X", command=mult, **kwargs).grid(column=3, row=1)
-
-        ttk.Button(btns, text="4", command=four, **kwargs).grid(column=0, row=2)
-        ttk.Button(btns, text="5", command=five, **kwargs).grid(column=1, row=2)
-        ttk.Button(btns, text="6", command=six, **kwargs).grid(column=2, row=2)
-        ttk.Button(btns, text="-", command=sub, **kwargs).grid(column=3, row=2)
-
-        ttk.Button(btns, text="1", command=one, **kwargs).grid(column=0, row=3)
-        ttk.Button(btns, text="2", command=two, **kwargs).grid(column=1, row=3)
-        ttk.Button(btns, text="3", command=three, **kwargs).grid(column=2, row=3)
-        ttk.Button(btns, text="+", command=add, **kwargs).grid(column=3, row=3)
-
-        ttk.Button(btns, text="0", command=zero, **kwargs).grid(
+        ttk.Button(keypad, text="0", command=partial(self.update_disp, "0"), **kwargs).grid(
             column=0,
             row=4,
             columnspan=2,
             sticky=tkinter.W+tkinter.E
         )
-        ttk.Button(btns, text=".", command=decimal, **kwargs).grid(column=2, row=4)
-        ttk.Button(btns, text="=", command=self.calculate, **kwargs).grid(column=3, row=4)
+        ttk.Button(keypad, text=".", command=partial(self.update_disp, "."), **kwargs).grid(column=2, row=4)
+        ttk.Button(keypad, text="=", command=self.calculate, **kwargs).grid(column=3, row=4)
 
-        ttk.Button(btns, text="Quit", command=gui.destroy, **kwargs).grid(
+        ttk.Button(keypad, text="Quit", command=gui.destroy, **kwargs).grid(
             column=0,
             row=5,
             columnspan=4,
@@ -144,10 +91,52 @@ class Calculator:
         gui.rowconfigure((0,1), weight=1)  # make buttons stretch when window is resized
         gui.columnconfigure((0,2), weight=1)
 
+        self.label = label
         return gui
 
+    def update_disp(self, val):
+        new_text = ''.join([item for item in self.memory[1 - DISP_WIDTH:]]) + val
+        self.label.configure(text=new_text)
+        self.memory.append(val)
+        self.label.update()
+
+    def clear(self):
+        self.label.configure(text="")
+        self.memory.clear()
+        self.label.update()
+
     def calculate(self):
-        pass
+        # Parse integers
+        parsed_mem = []
+        while len(self.memory) > 0:
+            idx = 0
+            while idx < len(self.memory) and self.Operation(self.memory[idx]) is self.Operation.NUMBER:
+                idx += 1
+
+            arg = int(''.join(self.memory[:idx]))
+            parsed_mem.append(arg)
+
+            if idx < len(self.memory):
+                op = self.Operation(self.memory[idx])
+                parsed_mem.append(op)
+
+            self.memory = self.memory[idx + 1:]
+
+        print(parsed_mem)
+
+        # Parse decimals
+        while parsed_mem.count(self.Operation.DECIMAL) > 0:
+            idx = parsed_mem.index(self.Operation.DECIMAL)
+            new_val = float(f"{parsed_mem[idx - 1]}.{parsed_mem[idx + 1]}")
+            parsed_mem = parsed_mem[idx + 2:]
+            parsed_mem.insert(0, new_val)
+
+        print(parsed_mem)
+
+        # TODO: Parse parenthesis
+
+        # TODO: Perform calculations
+
 
 
 if __name__ == "__main__":
